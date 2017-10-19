afa5a950557a1250b78913a9a9c5b9c7dc7bd21c
Gustavo Batistic Ribeiro <gustavo.batistic@gmail.com>
2017-10-19 20:37:47 -0200

    Comecando classe controle

---
+++ src/poo2salao/Controle.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo2salao;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Controle {
+    private ArrayList<Usuario> usuarios = new ArrayList();
+    private ArrayList<Cliente> clientes = new ArrayList();
+    private ArrayList<Comanda> comandas = new ArrayList();
+    private ArrayList<Agenda> agendas = new ArrayList();
+    
+    
+}
