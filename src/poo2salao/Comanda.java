d466d644ab827b43d6a231a5351a2f502bf533dd
baroni <baroni@Vostro-5470>
2017-10-19 20:24:44 -0200

    Implementação parcial de Comanda.

--- src/poo2salao/Comanda.java
+++ src/poo2salao/Comanda.java
@@ -13,9 +13,59 @@ import java.util.ArrayList;
 public class Comanda {
     private long id;
     private String idCliente;
+    private double valorFinal = 0;
+    private ArrayList<String> produto = new ArrayList();
+    private ArrayList<Long> valor = new ArrayList();
     
+    public Comanda(String c){
+        this.idCliente = c;
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+	long x = timestamp.getTime();
+        this.id = x;
+    }
     
-    public Comanda(){
-        
+    public void incValorFinal (long v) {
+        this.valorFinal += v;
+    }
+    
+    public boolean add(String p, int q, long v){
+        for(int i = 0; i < q; i++) {
+            this.produto.add(p);
+            this.valor.add(v);
+            this.incValorFinal(v);
+        }
+        return true;
+    }
+    
+    public boolean remove(int q, String p){
+        int cont = 0;
+        for(int i = 0; i < produto.size(); i++) {
+            if(p.equals(produto.get(i))){
+                cont++;
+            }
+        }
+        if(cont > q){
+            return false;
+        } else {
+            for(int i = 0; i < produto.size(); i++) {
+                if(p.equals(produto.get(i)) && cont > 0){
+                    produto.remove(i);
+                    cont--;
+                    this.incValorFinal(-1*valor.get(i));
+                    valor.remove(i);
+                }
+            } 
+        } 
+        return true;
+    }
+    
+    public boolean remove(String p){
+        for(int i = 0; i < produto.size(); i++) {
+            if(p.equals(produto.get(i))){
+                this.incValorFinal((-1)*(valor.get(i)));
+                valor.remove(i);
+                produto.remove(i);
+            }
+        } return true;
     }
 }
