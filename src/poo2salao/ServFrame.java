828b1c9077fb43b3fd95a260061379697542a3f9
Guilherme Atihe de Oliveira <g.atihe@192.168.1.3>
2017-11-13 20:40:57 -0200

    Algumas telas

--- src/poo2salao/ServFrame.java
+++ src/poo2salao/ServFrame.java
@@ -121,7 +121,11 @@ public class ServFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+            HomeFrame home = new HomeFrame();
+            home.setNivel(this.nivelAcesso);
+            home.setUsuario(this.usuario);
+            home.setVisible(true);
+            dispose();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
